e6358a35300ee7d631c2ed2994a0f3a9a95228a0
Davidson Ratis <davidsonmakarios@gmail.com>
2024-07-11 17:43:53 -0300

    updated - confirmParticipant method to the ParticipantService class

--- src/main/java/com/davr7/journey_jotter/services/ParticipantService.java
+++ src/main/java/com/davr7/journey_jotter/services/ParticipantService.java
@@ -1,11 +1,28 @@
 package com.davr7.journey_jotter.services;
 
+import com.davr7.journey_jotter.domain.Participant;
 import com.davr7.journey_jotter.repositories.ParticipantRepository;
+import com.davr7.journey_jotter.services.exceptions.ParticipantNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class ParticipantService {
     @Autowired
     ParticipantRepository participantRepo;
+
+    public Participant confirmParticipant(UUID id) {
+        Optional<Participant> participant = participantRepo.findById(id);
+
+        if (participant.isEmpty()) {
+            throw new ParticipantNotFoundException();
+        }
+
+        Participant rawParticipant = participant.get();
+        rawParticipant.setIsConfirmed(true);
+        return participantRepo.save(rawParticipant);
+    }
 }
