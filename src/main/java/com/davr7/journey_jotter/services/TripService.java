bac1afb3f3a927d1c75ebc3804ddc7017292aa27
Davidson Ratis <davidsonmakarios@gmail.com>
2024-07-11 14:40:21 -0300

    updated - formatting dates with the DateUtils class

--- src/main/java/com/davr7/journey_jotter/services/TripService.java
+++ src/main/java/com/davr7/journey_jotter/services/TripService.java
@@ -1,5 +1,6 @@
 package com.davr7.journey_jotter.services;
 
+import com.davr7.journey_jotter.common.DateUtils;
 import com.davr7.journey_jotter.domain.Trip;
 import com.davr7.journey_jotter.dtos.TripDtoRequest;
 import com.davr7.journey_jotter.repositories.TripRepository;
@@ -8,8 +9,6 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -22,8 +21,8 @@ public class TripService {
         Trip newTrip = new Trip();
         BeanUtils.copyProperties(data, newTrip, "startsAt", "endsAt", "emailsToInvite");
         newTrip.setIsConfirmed(false);
-        newTrip.setStartsAt(LocalDateTime.parse(data.startsAt(), DateTimeFormatter.ISO_DATE_TIME));
-        newTrip.setEndsAt(LocalDateTime.parse(data.endsAt(), DateTimeFormatter.ISO_DATE_TIME));
+        newTrip.setStartsAt(DateUtils.parseIsoDateTime(data.startsAt()));
+        newTrip.setEndsAt(DateUtils.parseIsoDateTime(data.endsAt()));
         return tripRepo.save(newTrip);
     }
 
