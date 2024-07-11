9ef94b199c3b924e8a91f5ec44ee693cecfaa5b3
Davidson Ratis <davidsonmakarios@gmail.com>
2024-07-11 18:35:07 -0300

    updated - registerParticipantsToEvent method to the ParticipantService class

--- src/main/java/com/davr7/journey_jotter/services/ParticipantService.java
+++ src/main/java/com/davr7/journey_jotter/services/ParticipantService.java
@@ -1,11 +1,13 @@
 package com.davr7.journey_jotter.services;
 
 import com.davr7.journey_jotter.domain.Participant;
+import com.davr7.journey_jotter.domain.Trip;
 import com.davr7.journey_jotter.repositories.ParticipantRepository;
 import com.davr7.journey_jotter.services.exceptions.ParticipantNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -24,5 +26,10 @@ public class ParticipantService {
         Participant rawParticipant = participant.get();
         rawParticipant.setIsConfirmed(true);
         return participantRepo.save(rawParticipant);
+    }
+
+    public void registerParticipantsToEvent(List<String> emailList, Trip trip) {
+        List<Participant> participants = emailList.stream().map(email -> new Participant(email, trip)).toList();
+        participantRepo.saveAll(participants);
     }
 }
