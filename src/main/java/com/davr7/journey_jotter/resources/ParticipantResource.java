f34cb9f366e33fa9fd564fc61dc4e9d1cec64df6
Davidson Ratis <davidsonmakarios@gmail.com>
2024-07-11 17:50:28 -0300

    updated - handleConfirmParticipant method to the ParticipantResource class

--- src/main/java/com/davr7/journey_jotter/resources/ParticipantResource.java
+++ src/main/java/com/davr7/journey_jotter/resources/ParticipantResource.java
@@ -1,12 +1,22 @@
 package com.davr7.journey_jotter.resources;
 
+import com.davr7.journey_jotter.domain.Participant;
 import com.davr7.journey_jotter.services.ParticipantService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "participant")
 public class ParticipantResource {
     @Autowired
     ParticipantService participantServ;
+
+    @GetMapping(value = "/{id}/confirm")
+    public ResponseEntity<Participant> handleConfirmParticipant(@PathVariable UUID id){
+        Participant participant = participantServ.confirmParticipant(id);
+        return ResponseEntity.ok().body(participant);
+    }
 }
