a73502bea711253d0a7e3fb248cbd2d727a1682a
Davidson Ratis <davidsonmakarios@gmail.com>
2024-07-11 12:12:06 -0300

    first commit

---
+++ src/main/java/com/davr7/journey_jotter/JourneyJotterApplication.java
@@ -0,0 +1,13 @@
+package com.davr7.journey_jotter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JourneyJotterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JourneyJotterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davr7/journey_jotter/PlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davr7.journey_jotter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
