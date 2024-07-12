4b07c36fe30fa99c62dba097e2f48a941bdc5d31
Davidson Ratis <davidsonmakarios@gmail.com>
2024-07-12 11:16:07 -0300

    updated - handleFindAllParticipantsFromEvent method to the TripResource class

--- src/main/java/com/davr7/journey_jotter/resources/TripResource.java
+++ src/main/java/com/davr7/journey_jotter/resources/TripResource.java
@@ -1,6 +1,7 @@
 package com.davr7.journey_jotter.resources;
 
 import com.davr7.journey_jotter.domain.Trip;
+import com.davr7.journey_jotter.dtos.ParticipantEventDto;
 import com.davr7.journey_jotter.dtos.TripDtoRequest;
 import com.davr7.journey_jotter.services.ParticipantService;
 import com.davr7.journey_jotter.services.TripService;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -45,5 +47,11 @@ public class TripResource {
     public ResponseEntity<Trip> handleConfirmTrip(@PathVariable UUID id){
         Trip trip = tripServ.confirmTrip(id);
         return ResponseEntity.ok().body(trip);
+    }
+
+    @GetMapping(value = "/{id}/participants")
+    public ResponseEntity<List<ParticipantEventDto>> handleFindAllParticipantsFromEvent(@PathVariable UUID id){
+        List<ParticipantEventDto> participants =  participantServ.findAllParticipantsFromEvent(id);
+        return ResponseEntity.ok().body(participants);
     }
 }
