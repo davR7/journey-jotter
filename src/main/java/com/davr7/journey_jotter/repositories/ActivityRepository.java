6a837a4a6bccf3511134db4d2c27819e34e872b8
Davidson Ratis <davidsonmakarios@gmail.com>
2024-07-12 15:39:46 -0300

    updated - findByTripId method to the ActivityRepository interface

--- src/main/java/com/davr7/journey_jotter/repositories/ActivityRepository.java
+++ src/main/java/com/davr7/journey_jotter/repositories/ActivityRepository.java
@@ -3,7 +3,9 @@ package com.davr7.journey_jotter.repositories;
 import com.davr7.journey_jotter.domain.Activity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ActivityRepository extends JpaRepository<Activity, UUID> {
+    List<Activity> findByTripId(UUID tripId);
 }
