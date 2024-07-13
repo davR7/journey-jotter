284aea387ee00906f62121c24ba53c32a384a45d
Davidson Ratis <davidsonmakarios@gmail.com>
2024-07-13 12:02:25 -0300

    updated - createNoteToEvent method to the NoteService class

--- src/main/java/com/davr7/journey_jotter/services/NoteService.java
+++ src/main/java/com/davr7/journey_jotter/services/NoteService.java
@@ -1,11 +1,25 @@
 package com.davr7.journey_jotter.services;
 
+import com.davr7.journey_jotter.domain.Note;
+import com.davr7.journey_jotter.domain.Trip;
+import com.davr7.journey_jotter.dtos.NoteCreateDto;
 import com.davr7.journey_jotter.repositories.NoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class NoteService {
     @Autowired
     NoteRepository noteRepo;
+
+    @Autowired
+    TripService tripServ;
+
+    public Note createNoteToEvent(UUID tripId, NoteCreateDto data){
+        Trip trip = tripServ.findTripById(tripId);
+        Note note = new Note(data, trip);
+        return noteRepo.save(note);
+    }
 }
