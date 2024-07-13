a442b0e2b3d7672070eddc1b8a2012df207c1ecc
Davidson Ratis <davidsonmakarios@gmail.com>
2024-07-13 10:49:34 -0300

    created - Note entity class

---
+++ src/main/java/com/davr7/journey_jotter/domain/Note.java
@@ -0,0 +1,34 @@
+package com.davr7.journey_jotter.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "notes")
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String url;
+
+    @ManyToOne
+    @JoinColumn(name="trip_id", nullable = false)
+    private Trip trip;
+}
