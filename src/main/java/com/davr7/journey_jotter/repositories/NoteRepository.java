12a5a4ea35cc88d0edf4f7bc9ef655d74fb9b3c1
Davidson Ratis <davidsonmakarios@gmail.com>
2024-07-13 12:28:04 -0300

    updated - findByTripId method to the NoteRepository interface

--- src/main/java/com/davr7/journey_jotter/repositories/NoteRepository.java
+++ src/main/java/com/davr7/journey_jotter/repositories/NoteRepository.java
@@ -1,9 +1,12 @@
 package com.davr7.journey_jotter.repositories;
 
 import com.davr7.journey_jotter.domain.Note;
+import com.davr7.journey_jotter.domain.Participant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface NoteRepository extends JpaRepository<Note, UUID> {
+    List<Note> findByTripId(UUID tripId);
 }
