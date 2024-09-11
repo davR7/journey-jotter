8114e171faf7403fe47c949a281374d5d69d9f87
Davidson Ratis <davidsonmakarios@gmail.com>
2024-09-11 11:57:41 -0300

    created - User entity class

---
+++ src/main/java/com/davr7/journey_jotter/domain/User.java
@@ -0,0 +1,31 @@
+package com.davr7.journey_jotter.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    public User(UUID id) {
+        this.id = id;
+    }
+}
